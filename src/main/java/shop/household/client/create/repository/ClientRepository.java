18c9a0fff7df3ec05d117d60248654cd5e6528f7
kenyetta <kenyetta@student.21-school.ru>
2024-08-06 22:05:33 +0300

    Revert "Начальный шаблон, версия 0.0.1"
    This reverts commit 15d189d81e5c1c108bece12bec599a60d39884a2.

--- src/main/java/shop/household/client/create/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package shop.household.client.create.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import shop.household.client.create.entity.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
