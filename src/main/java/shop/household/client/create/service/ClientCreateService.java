4b5aafc803ef8914cce698b127d31ad5d8dee117
kenyetta <kenyetta@student.21-school.ru>
2024-08-06 22:05:27 +0300

    Revert "Поправил ответ, добавил мапперы"
    This reverts commit eeaf6a5d33ca2f3c822b99cbaa9a42794316d797.

--- src/main/java/shop/household/client/create/service/ClientCreateService.java
+++ src/main/java/shop/household/client/create/service/ClientCreateService.java
@@ -2,10 +2,14 @@ package shop.household.client.create.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import shop.household.client.create.mapper.ClientMapper;
+import shop.household.client.create.entity.Client;
 import shop.household.client.create.repository.ClientRepository;
 import shop.household.model.ClientCreateRequestDto;
 import shop.household.model.ClientCreateResponseDto;
+import shop.household.model.ClientDto;
+import shop.household.model.ErrorDto;
+
+import java.sql.Timestamp;
 
 @Service
 @RequiredArgsConstructor
@@ -13,10 +17,27 @@ public class ClientCreateService {
 
     private final ClientRepository clientRepository;
 
-    public ClientCreateResponseDto create(ClientCreateRequestDto clientDto) throws Exception {
-        var client = clientRepository.save(ClientMapper.INSTANCE.requestDtoToClient(clientDto));
+    public ClientCreateResponseDto create(ClientCreateRequestDto clientDto) {
+        var client = clientRepository.save(new Client()
+                .setFirstname(clientDto.getFirstname())
+                .setLastname(clientDto.getLastname())
+                .setPhone(clientDto.getPhone())
+                .setPhone_extra(clientDto.getPhoneExtra())
+                .setEmail(clientDto.getEmail())
+                .setAddress(clientDto.getAddress())
+                .setCreate_at(new Timestamp(System.currentTimeMillis())));
         return new ClientCreateResponseDto()
                 .status(true)
-                .client(ClientMapper.INSTANCE.clientToClientDto(client));
+                .client(new ClientDto()
+                        .id(client.getId())
+                        .firstname(client.getFirstname())
+                        .lastname(client.getLastname())
+                        .phone(client.getPhone())
+                        .phoneExtra(client.getPhone_extra())
+                        .email(client.getEmail())
+                        .address(client.getAddress()))
+                .error(new ErrorDto()
+                        .code(200)
+                        .message("OK"));
     }
 }
